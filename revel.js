c76937af6136d48c0be00a0f5f01eb16a3cb2f41
LindsonV <lindson.dev@gmail.com>
2024-07-23 02:39:50 -0300

    Site cliente Mirela, segunda opção.

---
+++ revel.js
@@ -0,0 +1,57 @@
+
+window.revel = ScrollReveal({reset:true})
+
+//pagina 01
+
+revel.reveal('.efeito-txt-topo', {
+    duration: 2000,
+    distance: '90px',
+    origin: 'left'
+})
+
+revel.reveal('.efeito-img-topo', {
+    duration: 2000,
+    distance: '90px',
+    origin: 'right'
+    
+})
+
+//pagina 02
+
+revel.reveal('.efeito-txt-pg2', {
+    duration: 2000,
+    distance: '90px',
+    origin: 'right'
+})
+
+revel.reveal('.efeito-img-pg2', {
+    duration: 2000,
+    distance: '90px',
+    origin: 'left'
+})
+
+//pagina 03
+revel.reveal('.efeito-txt-pg3', {
+    duration: 2000,
+    distance: '90px',
+    origin: 'right'
+})
+
+revel.reveal('.efeito-card-1', {
+    duration: 2000,
+    distance: '90px',
+    delay: 500,
+    origin: 'top'
+})
+revel.reveal('.efeito-card-2', {
+    duration: 2000,
+    distance: '90px',
+    delay: 600,
+    origin: 'top'
+})
+revel.reveal('.efeito-card-3', {
+    duration: 2000,
+    distance: '90px',
+    delay: 700,
+    origin: 'top'
+})
---
+++ scirpt.js
@@ -0,0 +1,16 @@
+const imgs = document.getElementById("img");
+const img = document.querySelectorAll("#img img");
+
+let index = 0;
+
+function carrossel() {
+  index++;
+
+  if (index > img.length - 1) {
+    index = 0;
+  }
+
+  imgs.style.transform = `translateX(${-index * 500}px)`;
+}
+
+setInterval(carrossel, 1800);
